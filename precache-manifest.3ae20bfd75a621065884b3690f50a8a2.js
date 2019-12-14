6b209ee0a141d1a22212576d8f01112573aceee1
Jawad Ahmed <jawadahmedtaj@gmail.com>
2019-12-14 14:15:10 +0100

    Updates

---
+++ precache-manifest.3ae20bfd75a621065884b3690f50a8a2.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3371e46f60736cc60f4209982564cc63",
+    "url": "/WordstoMorseCode/index.html"
+  },
+  {
+    "revision": "9c6c4fe8abc2f7ac646e",
+    "url": "/WordstoMorseCode/static/css/main.49e99c6e.chunk.css"
+  },
+  {
+    "revision": "7ffe08f32260446d6ee7",
+    "url": "/WordstoMorseCode/static/js/2.58141c08.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/WordstoMorseCode/static/js/2.58141c08.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9c6c4fe8abc2f7ac646e",
+    "url": "/WordstoMorseCode/static/js/main.695b7207.chunk.js"
+  },
+  {
+    "revision": "bda06fb92a196cd576fc",
+    "url": "/WordstoMorseCode/static/js/runtime-main.f004a2a4.js"
+  },
+  {
+    "revision": "5a6361657f45eb5c5941a4297c63054f",
+    "url": "/WordstoMorseCode/static/media/A.5a636165.wav"
+  },
+  {
+    "revision": "3b27b51c056892b2daa253ee62ddbdf6",
+    "url": "/WordstoMorseCode/static/media/B.3b27b51c.wav"
+  },
+  {
+    "revision": "f00db532d631d51ae6b71f7b368c9824",
+    "url": "/WordstoMorseCode/static/media/C.f00db532.wav"
+  },
+  {
+    "revision": "7a0c1b40cebe515c94aa35c103fe136f",
+    "url": "/WordstoMorseCode/static/media/D.7a0c1b40.wav"
+  },
+  {
+    "revision": "187d1ac00e6a25ef7ee9efa7b90346a9",
+    "url": "/WordstoMorseCode/static/media/E.187d1ac0.wav"
+  },
+  {
+    "revision": "862f3439e68e6f3ded7fcfd257ca2d26",
+    "url": "/WordstoMorseCode/static/media/F.862f3439.wav"
+  },
+  {
+    "revision": "ad48508ee3781a33330cc61f9c27d414",
+    "url": "/WordstoMorseCode/static/media/G.ad48508e.wav"
+  },
+  {
+    "revision": "24968334340b100884dbd656e3b21147",
+    "url": "/WordstoMorseCode/static/media/H.24968334.wav"
+  },
+  {
+    "revision": "444e093af16a0dd67f6139162863853b",
+    "url": "/WordstoMorseCode/static/media/I.444e093a.wav"
+  },
+  {
+    "revision": "044b4c782d15949ffb22a323ddb274ee",
+    "url": "/WordstoMorseCode/static/media/J.044b4c78.wav"
+  },
+  {
+    "revision": "2ce889d24ef68abcac5820d4d2f4eff8",
+    "url": "/WordstoMorseCode/static/media/K.2ce889d2.wav"
+  },
+  {
+    "revision": "55616ad7a35c8b663dbb8802a6507332",
+    "url": "/WordstoMorseCode/static/media/L.55616ad7.wav"
+  },
+  {
+    "revision": "5d42445544de180ccbf0cb2e6a9daedf",
+    "url": "/WordstoMorseCode/static/media/M.5d424455.wav"
+  },
+  {
+    "revision": "d37a10ba4237459edea213408b10fbd0",
+    "url": "/WordstoMorseCode/static/media/N.d37a10ba.wav"
+  },
+  {
+    "revision": "cafd1f588fafa6e19aab6563a2500cbf",
+    "url": "/WordstoMorseCode/static/media/O.cafd1f58.wav"
+  },
+  {
+    "revision": "c9cefe7f36b0e2bd930fad2683886433",
+    "url": "/WordstoMorseCode/static/media/P.c9cefe7f.wav"
+  },
+  {
+    "revision": "43938f7baacd786fe59633f09c78eac0",
+    "url": "/WordstoMorseCode/static/media/Q.43938f7b.wav"
+  },
+  {
+    "revision": "9bd7c4c1ec0c6f5bea3147c4bf6fabcc",
+    "url": "/WordstoMorseCode/static/media/R.9bd7c4c1.wav"
+  },
+  {
+    "revision": "0c1516bdbf9a8cd71960faa962901464",
+    "url": "/WordstoMorseCode/static/media/S.0c1516bd.wav"
+  },
+  {
+    "revision": "ae96ba60cf93ceadfa56f946db808438",
+    "url": "/WordstoMorseCode/static/media/T.ae96ba60.wav"
+  },
+  {
+    "revision": "bc56b595dd86d45262be90e2d5e29be5",
+    "url": "/WordstoMorseCode/static/media/U.bc56b595.wav"
+  },
+  {
+    "revision": "4fe691bf3b1350e353b2a040f330391a",
+    "url": "/WordstoMorseCode/static/media/V.4fe691bf.wav"
+  },
+  {
+    "revision": "33b6d4f75e91abb671c92553d215eba4",
+    "url": "/WordstoMorseCode/static/media/W.33b6d4f7.wav"
+  },
+  {
+    "revision": "d3910e8e5ea89c1fd55387c1c21d954a",
+    "url": "/WordstoMorseCode/static/media/X.d3910e8e.wav"
+  },
+  {
+    "revision": "2ce53ecb32adf6c5e0c7efd913348850",
+    "url": "/WordstoMorseCode/static/media/Y.2ce53ecb.wav"
+  },
+  {
+    "revision": "942c27d65e0de4aa2825fd74384d25e7",
+    "url": "/WordstoMorseCode/static/media/Z.942c27d6.wav"
+  }
+]);
